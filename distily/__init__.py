69c7048e86292e54d89f05056c1891a85473a29a
Andrew Lapp <andrew@github.rew.la>
2024-08-11 11:53:52 -0500

    revert __version__

--- distily/__init__.py
+++ distily/__init__.py
@@ -1,5 +1,2 @@
 from . import args, benchmark, cli, distillation_trainer, objectives, metrics
 from . import tinyllama_bitnet_utils  # TODO: Remove, replace with a good bitnet library
-
-import importlib.metadata
-__version__ = importlib.metadata.version("mypackage")
