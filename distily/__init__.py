3293a73f5da870be9e1b8f8b27d88e48e1ec9b46
Andrew Lapp <andrew@github.rew.la>
2024-08-11 12:29:57 -0500

    try fixing kl div

--- distily/__init__.py
+++ distily/__init__.py
@@ -2,4 +2,4 @@ from . import args, benchmark, cli, distillation_trainer, objectives, metrics
 from . import tinyllama_bitnet_utils  # TODO: Remove, replace with a good bitnet library
 
 import importlib.metadata
-__version__ = importlib.metadata.version("mypackage")
+__version__ = importlib.metadata.version("distily")
