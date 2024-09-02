7ce102313683e61535bbef37654c8f6ff3f36ff6
Andrew Lapp <andrew@github.rew.la>
2024-09-02 11:34:15 -0400

    updating gradient logging for trainer

--- distily/distillation_trainer.py
+++ distily/distillation_trainer.py
@@ -222,10 +222,11 @@ class DistillationTrainer(transformers.Trainer):
             stats["grad_prev_similarity"] = _bit_tensor_sum(equal_bits) / (equal_bits.numel() * 8)
         self._prev_grad_sign = grad_sign
 
-        stats["_grad_norm"] = torch.norm(
-            torch.stack([torch.norm(p.grad) for p in model.parameters()]),
-            2.0
-        ).item()
+        stats["_grad_norm"] = torch.cat([
+            param.grad.detach().flatten()
+            for param in model.parameters()
+            if param.grad is not None
+        ]).norm().item()
 
         self._extra_stats.append(stats)
 
