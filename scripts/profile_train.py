8c98ef12ccff7bc7e56d03f098e80b66c06e6be9
Andrew Lapp <andrew@github.rew.la>
2024-09-22 12:08:00 -0400

    fix to profiling - will test later

--- scripts/profile_train.py
+++ scripts/profile_train.py
@@ -1,4 +1,4 @@
-from torch.profiler import profile, record_function, ProfilerActivity, schedule
+from torch.profiler import profile, record_function, ProfilerActivity, schedule, tensorboard_trace_handler
 from distily.distillation_trainer import DistillationTrainer
 from transformers import TrainerCallback
 
@@ -19,20 +19,12 @@ class ProfCallback(TrainerCallback):
 def profile_trainer_train(trainer):
     with profile(
         activities=[ProfilerActivity.CPU, ProfilerActivity.CUDA],
-        schedule=schedule(skip_first=4, wait=0, warmup=2, active=4, repeat=1),
+        schedule=schedule(skip_first=16, wait=2, warmup=2, active=2, repeat=1),
         with_stack=True,
+        on_trace_ready=tensorboard_trace_handler(trainer.args.output_dir)
     ) as prof:
         trainer.add_callback(ProfCallback(prof=prof))
         trainer.train()
-
-        print("\nTop 40 operations by CPU time:")
-        print(prof.key_averages().table(
-            sort_by="self_cpu_time_total",
-            row_limit=40,
-            max_src_column_width=150,
-            top_level_events_only=False
-        ))
-
         print("\nTop 40 operations by CUDA time:")
         print(prof.key_averages().table(
             sort_by="self_cuda_time_total",
@@ -41,14 +33,11 @@ def profile_trainer_train(trainer):
             top_level_events_only=False
         ))
 
-        # Export Chrome trace
-        prof.export_chrome_trace("train_trace.json")
-
 
 def get_trainer(**replacements):
     trainer_kwargs = dict(
         teacher_model_name_or_path="HuggingFaceTB/SmolLM-135M",
-        dataset_sample_size=64,
+        dataset_sample_size=128,
         per_device_train_batch_size=4,
         eval_strategy="no",
         eval_on_start=False,
